6092bb715870f3126656ca963c9298ffe56c6aa1
gerrickb <lsgenether@gmail.com>
2022-10-10 00:35:00 -0700

    Set up Player and TicTac class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,5 +1,4 @@
-module TicTac
-
+class TicTac
   WIN_COMBO = [
     [0, 1, 2],
     [3, 4, 5],
@@ -10,8 +9,15 @@ module TicTac
     [0, 4, 8],
     [2, 4, 6],
   ]
+  @@choices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   
+  def self.play_game(p_1, p_2)
+  end
 end
 
 class Player
+  def initialize(name)
+    @name = name
+    @choice = []
+  end
 end
