e7bb85e4cfa34f1fb326c613fa6290db30578c90
gerrickb <lsgenether@gmail.com>
2022-10-15 04:23:06 -0700

    Edit TicTac class and add winner_check method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -9,19 +9,36 @@ class TicTac
     [0, 4, 8],
     [2, 4, 6],
   ]
-  @@num = a
+  @@num = ''
   @@choices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   
   def self.play_game(p_1, p_2)
     puts "#{p_1.name} and #{p_2.name} want to play"
-    
+
     while @@choices.length != 0
-      until @@choices.include?(@@num)
-        puts "choose from #{@@choices}"
-        num = gets.chomp.to_i
-        player_1.choice << num
+      puts "#{p_1.name}'s turn, choose from #{@@choices}"
+      game(p_1)
+      #p p_1.choice
+      break if winner_check(p_1.name, p_1.choice) == true
+    end
+  end
+
+  def self.game(player)
+    @@num = gets.chomp.to_i
+    player.choice << @@num
+    p player.choice
+    @@choices.delete(@@num)
+  end
+
+  def self.winner_check(player, choice)
+    WIN_COMBO.each do |combo|
+      if combo.eql?(choice)
+        puts "#{player} has won the game"
+        return true
       end
     end
+  end
+
 end
 
 class Player
@@ -30,17 +47,9 @@ class Player
     @name = name
     @choice = []
   end
-
-  def choose_number
-    @choice << gets.chomp.to_i
-  end
-
 end
 
 player_1 = Player.new('Player 1')
 player_2 = Player.new('Player 2')
-p player_1
-puts player_1.name
-puts player_1.choice
 
 TicTac.play_game(player_1, player_2)
